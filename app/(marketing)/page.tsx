c9febed8910bdb1a86faa445be31c43df08d6c58
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-15 17:44:31 +0300

    Map the navbar items

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
 const MarketingPage = () => {
-  return <div className="p-8 md:p-4 "></div>;
+  return <div className="p-8 md:p-4 ">Hello!</div>;
 };
 
 export default MarketingPage;
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import * as React from "react";
 
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 import { Icons } from "./icons";
 import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 import { MainNavItem } from "@/types";
 
 interface MainNavProps {
@@ -12,6 +16,8 @@ interface MainNavProps {
 }
 
 const MainNav = ({ items, children }: MainNavProps) => {
+  const segment = useSelectedLayoutSegment();
+  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="hidden items-center space-x-2 md:flex">
@@ -20,6 +26,25 @@ const MainNav = ({ items, children }: MainNavProps) => {
           {siteConfig.name}
         </span>
       </Link>
+      {items?.length ? (
+        <nav className="hidden gap-6 md:flex">
+          {items?.map((item) => (
+            <Link
+              href={item.disabled ? "#" : item.href}
+              className={cn(
+                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm;text-sm",
+                item.href.startsWith(`/${segment}`)
+                  ? "text-foreground"
+                  : "text-foreground/60",
+                item.disabled && "cursor-not-allowed opacity-80"
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      ) : null}
+      <button></button>
     </div>
   );
 };
