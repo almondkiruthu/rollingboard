74387bb49f25d32920ae4f21b905aba503dd3fe9
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-18 14:28:30 +0300

    Refactor-config site url

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: Siteconfig = {
   name: "Rollingboard",
   description:
     "An open-source project management tool powered by Next.js for an efficient and collaborative workflow.",
-  url: "http://localhost:3000/",
+  url: "https://rollingboard.vercel.app",
   links: {
     github: "https://github.com/almondkiruthu/rollingboard",
     twitter: "https://twitter.com/almondkiruthu",
