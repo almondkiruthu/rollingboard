385f11d4a4b2e57e361495f81a61cb53b137b4a2
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-23 23:10:03 +0300

    Add prisma adapter- to auth options

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,8 @@
+import { db } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
   providers: [],
 };
