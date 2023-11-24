bfd7986c7fb84bf89dea477437ef6086417e6c40
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-24 18:33:19 +0300

    Refactor- github env files

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,12 +3,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
-  throw new Error(
-    "Environment variables GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET are not set"
-  );
-}
-
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   session: {
@@ -16,8 +10,8 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
 };
