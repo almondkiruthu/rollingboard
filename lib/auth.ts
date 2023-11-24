22ed2058fea2043615078beb3d0892938ddc1004
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-24 18:15:31 +0300

    Configure- github provider in auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,23 @@
 import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+  throw new Error(
+    "Environment variables GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET are not set"
+  );
+}
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
   },
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+  ],
 };
