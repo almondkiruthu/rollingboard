00f981db9002dfff9631aa788ba951cc14e70163
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-25 23:48:31 +0300

    Refactor- middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -54,5 +54,4 @@ export const authOptions: NextAuthOptions = {
       };
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
 };
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
-  async function middleware(req: NextRequest) {
+  async function middleware(req) {
     const token = await getToken({ req });
     const isAuth = !!token;
     const isAuthPage =
@@ -43,5 +43,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/editor/:path*", "/login", "/register"],
+  matcher: ["/dashboard/:path*", "/login", "/register"],
 };
