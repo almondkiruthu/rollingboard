9184ce54a9c800141405120a73024fc04e4d0982
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-27 15:11:44 +0300

    Create- db query to fetch tasks on dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,32 @@
-const DashBoardPage = () => {
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+
+export const metdata = {
+  title: "Dashboard",
+};
+
+const DashBoardPage = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in" || "/sign-up");
+  }
+  const tasks = await db.task.findMany({
+    where: {
+      creatorId: userId,
+    },
+    select: {
+      id: true,
+      title: true,
+      published: true,
+      description: true,
+      createdAt: true,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  });
   return <div>DashBoard Page</div>;
 };
 
