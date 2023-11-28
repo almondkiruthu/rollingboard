65de3eccf09473fb2928d3f2f35d7a05d1551649
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-28 23:18:02 +0300

    Create- new layout for the organization

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,7 +1,5 @@
 import Navbar from "./_components/navbar";
-import DashBoardNav from "@/components/nav";
 import SiteFooter from "@/components/site-footer";
-import { dashboardConfig } from "@/config/dashboard";
 
 interface DashBoardLayoutProps {
   children?: React.ReactNode;
@@ -9,17 +7,10 @@ interface DashBoardLayoutProps {
 
 const DashBoardLayout = ({ children }: DashBoardLayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col space-y-6">
+    <div className="h-full">
       <Navbar />
-      <div className="container grid flex-1 gap-12">
-        {/* <aside className="hidden w-[200px] flex-col md:flex">
-          <DashBoardNav items={dashboardConfig.sideBarNav} />
-        </aside> */}
-        <main className="flex w-full flex-1 flex-col overflow-x-hidden overflow-y-auto mx-auto md:ml-[200px]">
-          {children}
-        </main>
-      </div>
-      <SiteFooter className="border-t" />
+      {children}
+      {/* TODO: add site footer */}
     </div>
   );
 };
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,67 +1,9 @@
-import { redirect } from "next/navigation";
-
-import EmptyPlaceHolder from "@/components/empty-placeholder";
-import DashBoardHeader from "@/components/header";
-import { Icons } from "@/components/icons";
-import DashBoardShell from "@/components/shell";
-import TaskItem from "@/components/task-item";
-import { Button } from "@/components/ui/button";
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
-
 export const metadata = {
   title: "Dashboard",
 };
 
-const OrganizationIdPage = async () => {
-  const { userId } = auth();
-  if (!userId) {
-    redirect("/sign-in" || "/sign-up");
-  }
-  const tasks = await db.task.findMany({
-    where: {
-      creatorId: userId,
-    },
-    select: {
-      id: true,
-      title: true,
-      published: true,
-      description: true,
-      createdAt: true,
-    },
-    orderBy: {
-      updatedAt: "desc",
-    },
-  });
-  return (
-    <DashBoardShell>
-      <DashBoardHeader
-        header="Projects"
-        text="Create and manage projects"
-      ></DashBoardHeader>
-      <div>
-        {tasks?.length ? (
-          <div className="divide-y divide-border rounded-md border">
-            {tasks.map((task) => (
-              <TaskItem key={task.id} task={task} />
-            ))}
-          </div>
-        ) : (
-          <EmptyPlaceHolder className="bg-slate-50/30 py-8 dark:bg-transparent">
-            <EmptyPlaceHolder.Icon name="project" />
-            <EmptyPlaceHolder.Title>No Projects created</EmptyPlaceHolder.Title>
-            <EmptyPlaceHolder.Description>
-              You don&apos;t have any projects yet. Start creating tasks.
-            </EmptyPlaceHolder.Description>
-            <Button className="flex items-center" variant="primary">
-              <Icons.add className="mr-2 h-4 w-4" />
-              New Project
-            </Button>
-          </EmptyPlaceHolder>
-        )}
-      </div>
-    </DashBoardShell>
-  );
+const OrganizationIdPage = () => {
+  return <div>Organization id page</div>;
 };
 
 export default OrganizationIdPage;
