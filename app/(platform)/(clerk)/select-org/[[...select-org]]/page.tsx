9d351f346ebca9bb7b2d23127b06719b73115058
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-29 14:11:17 +0300

    Create- organization page

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganizationPage() {
+  return (
+    <OrganizationList
+      hidePersonal={true}
+      afterCreateOrganizationUrl="/organization/:slug"
+      afterSelectOrganizationUrl="/organization/:slug"
+    />
+  );
+}
