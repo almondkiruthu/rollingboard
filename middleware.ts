9e68e25de18ba79315b58bb8e7cbdfb607fd703d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-29 15:22:05 +0300

    Refactor- middleware

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,32 @@ import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgSlug) {
+        path = `/organization/${auth.orgSlug}`;
+      }
+
+      const organizationSelection = new URL(path, req.url);
+      return NextResponse.redirect(organizationSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (
+      auth.userId &&
+      !auth.orgSlug &&
+      req.nextUrl.pathname !== "/select-org"
+    ) {
+      const organizationSelection = new URL("/select-org", req.url);
+
+      return NextResponse.redirect(organizationSelection);
+    }
+  },
 });
 
 export const config = {
