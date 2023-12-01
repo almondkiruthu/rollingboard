921f5ed3fb813d899b46f413ed751479cb36ffa4
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-01 17:24:38 +0300

    Create- project creation validation

---
+++ actions/create-project/schema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const CreateProject = z.object({
+  title: z
+    .string({
+      required_error: "Title is Required",
+      invalid_type_error: "Title is Required",
+    })
+    .min(3, {
+      message: "Title is too short",
+    }),
+  image: z.string({
+    required_error: "Image is Required",
+    invalid_type_error: "Image is Required",
+  }),
+});
