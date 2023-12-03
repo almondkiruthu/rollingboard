c07b261118e67d4f0c50e249efc34f231e18dc54
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-03 18:14:29 +0300

    Create- generic form input component

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -2,7 +2,9 @@
 
 import React from "react";
 
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 import { useFormStatus } from "react-dom";
 
 interface FormInputProps {
@@ -37,8 +39,34 @@ export const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(
     const { pending } = useFormStatus();
     return (
       <div className="space-y-2">
-        <div className="space-y-1">{label ? <Label></Label> : null}</div>
+        <div className="space-y-1">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+          ) : null}
+
+          <Input
+            onBlur={onBlur}
+            defaultValue={defaultValue}
+            ref={ref}
+            required={required}
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
+            className={cn("text-sm px-2 py-1", className)}
+            aria-aria-describedby={`${id}-error`}
+          />
+        </div>
+        {/* TODO: FormErrors component */}
       </div>
     );
   }
 );
+
+FormInput.displayName = "FormInput";
