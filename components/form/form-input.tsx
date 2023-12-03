47f490805e87946478fc5798b5f94af43af0493d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-03 18:07:51 +0300

    Refactor- ts config file

---
+++ components/form/form-input.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+
+import { Label } from "@/components/ui/label";
+import { useFormStatus } from "react-dom";
+
+interface FormInputProps {
+  id: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  defaultValue?: string;
+  onBlur?: () => void;
+}
+
+export const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(
+  (
+    {
+      id,
+      label,
+      type,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      className,
+      defaultValue,
+      onBlur,
+    },
+    ref
+  ) => {
+    const { pending } = useFormStatus();
+    return (
+      <div className="space-y-2">
+        <div className="space-y-1">{label ? <Label></Label> : null}</div>
+      </div>
+    );
+  }
+);
