ce773d09c6c710db79406746dc03b74fbd20b73e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-05 23:17:12 +0300

    Chores- create form-errors component

---
+++ components/form/form-errors.tsx
@@ -0,0 +1,31 @@
+import { Icons } from "../icons";
+
+interface FormErrorsProps {
+  id: string;
+  errors?: Record<string, string[] | undefined>;
+}
+
+const FormErrors = ({ id, errors }: FormErrorsProps) => {
+  if (!errors) {
+    return null;
+  }
+  return (
+    <div
+      id={`${id}-error`}
+      aria-live="polite"
+      className="mt-2 text-xs text-rose-500"
+    >
+      {errors?.[id]?.map((error: string) => (
+        <div
+          key={error}
+          className="flex items-center font-medium p-2 border border-rose-500/50
+        bg-rose-500/10 rounded-sm"
+        >
+          <Icons.closeIcon className="h-4 w-4 mr-2" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FormErrors;
