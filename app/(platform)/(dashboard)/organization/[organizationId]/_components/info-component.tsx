9d4820c6f1cd148e37f351914e90d9ed55b71d69
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-06 14:05:11 +0300

    Style: render org name and image

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info-component.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info-component.tsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+
+import { Icons } from "@/components/icons";
+import { useOrganization } from "@clerk/nextjs";
+
 const Info = () => {
-  return <div></div>;
+  const { organization, isLoaded } = useOrganization();
+  return (
+    <div className="flex items-center gap-x4">
+      <div className="w-8 h-8 relative">
+        <Image
+          fill
+          src={organization?.imageUrl as string}
+          alt="Organization"
+          className="rounded-md object-cover"
+        />
+      </div>
+      <div className="space-y-1">
+        <p className="font-semibold tracking-tight text-xl">
+          {organization?.name}
+        </p>
+        <div className="flex items-center text-xs text-muted-foreground">
+          <Icons.billing className="h-3 w-3 mr-2" />
+          You are on the Free Plan
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
