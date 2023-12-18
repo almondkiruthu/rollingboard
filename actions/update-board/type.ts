5050a4b46817b2d81b25634ce262396216377dba
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-18 10:39:50 +0300

    refactor: update board action types

--- actions/update-board/type.ts
+++ actions/update-board/type.ts
@@ -1,13 +1,7 @@
+import { UpdateBoard } from "./schema";
+import { ActionState } from "@/lib/create-safe-action";
+import { Project } from "@prisma/client";
 import { z } from "zod";
 
-export const UpdateBoard = z.object({
-  title: z
-    .string({
-      required_error: "Title is required",
-      invalid_type_error: "Title is required",
-    })
-    .min(3, {
-      message: "Title is too short",
-    }),
-  id: z.string(),
-});
+export type InputType = z.infer<typeof UpdateBoard>;
+export type ReturnType = ActionState<InputType, Project>;
