ebfcec507dd4bff5181878286a5ce0b577610d24
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-19 23:39:33 +0300

    chore: create update board logic

---
+++ actions/update-board/index.ts
@@ -0,0 +1,36 @@
+import { InputType, ReturnType } from "./type";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, id } = data;
+
+  let project;
+
+  try {
+    project = await db.project.update({
+      where: {
+        id,
+        orgId,
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+        error: "Failed to update"
+    }
+  }
+
+  revalidatePath
+};
