9190f9ffee701ab7789c3576797bb8dd54fd4215
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-12-26 16:02:14 +0300

    refactor: generate orgId metadata

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,7 +1,17 @@
 import { OrgControl } from "./_components/org-control-component";
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
 
 interface OrganizationSlugLayoutProps {
   children: React.ReactNode;
+}
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
 }
 
 const OrganizationSlugLayout = ({ children }: OrganizationSlugLayoutProps) => {
