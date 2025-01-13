85369f4c738ec460345dbeaf61830693327f025b
Almond Kiruthu <almondkiruthu@gmail.com>
2025-01-13 16:29:04 +0300

    chore: :wrench: set up react tanstack query

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,16 +1,19 @@
-import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { TailwindIndicator } from '@/components/tailwind-indicator'
+import { QueryProvider } from './query-provider'
 
 interface ProvidersProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const Providers = ({ children }: ProvidersProps) => {
   return (
     <>
-      {children}
-      <TailwindIndicator />
+      <QueryProvider>
+        {children}
+        <TailwindIndicator />
+      </QueryProvider>
     </>
-  );
-};
+  )
+}
 
-export default Providers;
+export default Providers
