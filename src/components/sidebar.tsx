577a7ad42291f29c6b3272bfe2e9cdd338e0ada4
Almond Kiruthu <almondkiruthu@gmail.com>
2025-01-15 12:02:09 +0300

    feat: :sparkles: setup dashboard layout

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+// import { Projects } from "./projects";
+// import { Navigation } from "./navigation";
+import { DottedSeparator } from './dotted-separator'
+import { WorkspaceSwitcher } from './workspace-switcher'
+import { Icons } from './icons'
+import { siteConfig } from '@/config/site'
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full w-full bg-neutral-100 p-4">
+      <Link href="/" className="flex items-center space-x-2">
+        <Icons.mainLogo className="hidden md:inline-block" />
+        <span className="hidden font-bold md:inline-block">
+          {siteConfig.name}
+        </span>
+      </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
+      <DottedSeparator className="my-4" />
+      <Navigation />
+      <DottedSeparator className="my-4" />
+      <Projects />
+    </aside>
+  )
+}
