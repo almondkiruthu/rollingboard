bf1a1f0f96a80887aebafa0d92770fb2a971865e
Almond Kiruthu <almondkiruthu@gmail.com>
2025-01-30 10:08:33 +0300

    feat: :sparkles: create projects settings

--- src/features/projects/components/edit-project-form.tsx
+++ src/features/projects/components/edit-project-form.tsx
@@ -67,7 +67,7 @@ export const EditProjectForm = ({ onCancel, initialValues }: EditProjectFormProp
       param: { projectId: initialValues.$id },
     }, {
       onSuccess: () => {
-        window.location.href = `/workspaces/${initialValues.workspaceId}`;
+        window.location.href = `/dashboard/workspaces/${initialValues.workspaceId}`;
       },
     });
   };
@@ -96,7 +96,7 @@ export const EditProjectForm = ({ onCancel, initialValues }: EditProjectFormProp
       <DeleteDialog />
       <Card className="w-full h-full border-none shadow-none">
         <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
-          <Button size="sm" variant="secondary" onClick={onCancel ? onCancel : () => router.push(`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}`)}>
+          <Button size="sm" variant="secondary" onClick={onCancel ? onCancel : () => router.push(`/dashboard/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}`)}>
             <ArrowLeftIcon className="size-4 mr-2" />
             Back
           </Button>
